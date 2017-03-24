d92cc7be3dc078527ebf8c8fed24213e4bfd2724
Ari-Pekka Härkönen <ari-pekka.harkonen@aalto.fi>
2017-03-24 20:47:38 +0200

    UI is working

--- app/src/main/java/fi/me/tlpm/MainActivity.java
+++ app/src/main/java/fi/me/tlpm/MainActivity.java
@@ -1,13 +1,45 @@
 package fi.me.tlpm;
 
+import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import java.util.*;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+
+public class MainActivity extends ListActivity {
+
+
+        //LIST OF ARRAY STRINGS WHICH WILL SERVE AS LIST ITEMS
+        ArrayList<String> listItems=new ArrayList<String>();
+
+
+        //DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE LISTVIEW
+        ArrayAdapter<String> adapter;
+
+        //RECORDING HOW MANY TIMES THE BUTTON HAS BEEN CLICKED
+        int clickCounter=0;
+
+        @Override
+        public void onCreate(Bundle icicle) {
+            super.onCreate(icicle);
+            setContentView(R.layout.activity_main);
+            adapter=new ArrayAdapter<String>(this,
+                    android.R.layout.simple_list_item_1,
+                    listItems);
+            setListAdapter(adapter);
+        }
+
+        //METHOD WHICH WILL HANDLE DYNAMIC INSERTION
+        public void addItems(View v) {
+            
+            adapter.add("New Item");
+        }
+
+
     }
-}
+
